2d527c46e4153ca311f0cb113c8b5a4692000c74
HHSNZ <hh.stobo@gmail.com>
2020-05-23 16:41:48 +1200

    linked to all the listed companies

--- src/components/content/work-sections/CurrentWork.vue
+++ src/components/content/work-sections/CurrentWork.vue
@@ -6,46 +6,46 @@
                 <h3 class="company-title">Saturn Advice Ltd</h3>
                 <span class="job-title">Chairman and Shareholder</span>
             </a>
-            <div class="positions-list-item">
+            <a href="https://www.elevationcapital.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/elevation-capital-logo.png'" alt="Elevation Capital Logo">
                 <h3 class="company-title">Elevation Capital</h3>
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.lgfa.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/lgfa.png'" alt="Local Government Funding Agency Logo">
                 <h3 class="company-title">Local Government Funding Agency</h3>
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.precinct.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/precinct-properties-logo.png'" alt="Precinct Properties Ltd Logo">
                 <h3 class="company-title">Precinct Properties Ltd</h3>
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.aig.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/AIG-logo.png'" alt="AIG Insurance New Zealand Logo">
                 <h3 class="company-title">AIG Insurance New Zealand</h3>
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://appelloservices.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/apello-services-logo.png'" alt="Apello Services Ltd Logo">
                 <h3 class="company-title">Apello Services Ltd</h3>
                 <span class="job-title">Director</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="http://www.biomarine.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/biomarine-logo.png'" alt="Biomarine Ltd Logo">
                 <h3 class="company-title">Biomarine Ltd</h3>
                 <span class="job-title">Chairman and Shareholder</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.cressida.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/cressida-capital-logo.png'" alt="Cressida Capital Logo">
                 <h3 class="company-title">Cressida Capital</h3>
                 <span class="job-title">Board Advisor</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="http://www.urbanpartners.co.nz" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/current/urban-partners.png'" alt="Urban Partners NZ Logo">
                 <h3 class="company-title">Urban Partners</h3>
                 <span class="job-title">Board Advisor</span>
-            </div>
+            </a>
         </ul>
     </div>
 </template>
--- src/components/content/work-sections/PreviousWork.vue
+++ src/components/content/work-sections/PreviousWork.vue
@@ -1,41 +1,41 @@
 <template>
     <div>
         <ul class="positions-list">
-            <div class="positions-list-item">
+            <a href="https://www.fliway.com/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/previous/fliway-group-logo.png'" alt="Fliway Group Logo">
                 <h3 class="company-title">Fliway Group</h3>
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.bureau.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/previous/bureau-logo.png'" alt="Buereau Ltd Logo">
                 <h3 class="company-title">Bureau Ltd</h3> 
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.ocg.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/previous/ocg-logo.png'" alt="OCG Consulting Ltd Logo">
                 <h3 class="company-title">OCG Consulting Ltd</h3>
                 <span class="job-title">Chairman</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.callaghaninnovation.govt.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/previous/callaghan-innovation-logo.png'" alt="Industrial Resarch Ltd Logo">
                 <h3 class="company-title">Industrial Research Ltd</h3>
                 <span class="job-title">Director</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://nzgif.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img style="height: 80px; width: 125px;" class="company-logo" v-bind:src="'./images/company-logos/previous/NZGIF-logo.png'" alt="NZGIF Logo">
                 <h3 class="company-title">New Zealand Green Investment Finance Ltd</h3>
                 <span class="job-title">Advisor</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://treasury.govt.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img style="height: 90px;" class="company-logo" v-bind:src="'./images/company-logos/previous/treasury-logo.png'" alt="NZ Treasury Logo">
                 <h3 class="company-title">New Zealand Treasury</h3>
                 <span class="job-title">Advisor</span>
-            </div>
-            <div class="positions-list-item">
+            </a>
+            <a href="https://www.lgnz.co.nz/" target="_blank" style="text-decoration: none; color: inherit" class="positions-list-item">
                 <img class="company-logo" v-bind:src="'./images/company-logos/previous/lgnz-logo.png'" alt="Local Government New Zealand Logo">
                 <h3 class="company-title">Local Government New Zealand</h3>
                 <span class="job-title">Advisor</span>
-            </div>
+            </a>
         </ul>
     </div>
 </template>
